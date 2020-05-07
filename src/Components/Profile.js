b6ca54524d88c5daf7cbe92d39ebd8395ef9f05a
Matthew <matthew25kim@gmail.com>
2020-05-06 20:35:30 -0400

    profile card

--- src/Components/Profile.js
+++ src/Components/Profile.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import './Style/Profile.css'
 import profpic from './matt.jpg'
+import penn from './upenn.png'
 import {Container, Row, Card, Image} from 'react-bootstrap';
 
 class Profile extends React.Component {
@@ -13,9 +14,9 @@ class Profile extends React.Component {
                             <Image src={profpic} roundedCircle/>
                         </div>              
                         <div class="intro">
-                            <h1 class="name"> Hi, I'm <span class="my-name"> Matthew Kim.</span> </h1>
-                            <p class="desc"> I'm a sophomore studying CS at the University of Pennsylvania, interested in
-                            the applications of software development and data science. </p>
+                            <h1 class="name"> Hi, I'm Matthew Kim. </h1>
+                            <p class="desc"> I'm a junior studying computer science and data science at the <a class="penn-link" href="https://www.seas.upenn.edu/">University of Pennsylvania</a>, interested in
+                            exploring the applications of software development. </p>
                             {/* <p class="learning"> What I'm currently learning: <span class="react"> React </span> <i class="fab fa-react 5x"></i> </p> */}
                         </div>
                     </div>
