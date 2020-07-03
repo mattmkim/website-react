39b702b19c65f3062e1b118578b6999a6a2d078b
Matthew <matthew25kim@gmail.com>
2020-07-03 00:01:13 -0400

    refactored projects page

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -16,91 +16,122 @@ class Projects extends React.Component {
                 <Container className="project-container">
                     <h2 class="project-title">Projects</h2>
 
-                    <Card className="project-card">
-                        <div class="project-image-wrapper">
-                            <Image src={roads} className="image" />
-                        </div>
-                        <div class="info-wrapper">
-                            <h3><span class="project-name"> Safe Roads </span></h3>
-                            <p class="project-info">Final project for CIS 550: Database and Information Systems. Web app to explore the relationship between U.S. accident and weather data.</p>
-                            <p class="project-info"><span class="tech-used">Tech Used: </span>Javascript, Node, Express, HTML, CSS, SQL, MongoDB, Heroku </p>
-                            <Button href="https://github.com/mattmkim/safe-roads">View Project <i class="fas fa-external-link-alt"></i></Button>
-                        </div>
-                    </Card>
+                    <div class="project-row">
+                        <Card className="card-project">
+                            <div class="card-body">
+                                <div class="images">
+                                    <i class="far fa-folder-open fa-7x"></i>
+                                    <a href="https://github.com/mattmkim/safe-roads" target="_blank"> 
+                                        <i class="fab fa-github fa-5x"></i>
+                                    </a>
+                                </div>
+                                <div class="card-info">
+                                    <h4 class="card-title">Safe Roads</h4>
+                                    <p>Final project for CIS 550: Database and Information Systems. Web app to explore the relationship between U.S. accident and weather data.</p>
+                                    <p class="card-tech">Tech Used: Javascript, Node, Express, HTML, CSS, SQL, MongoDB, Heroku</p>
+                                </div>
+                            </div>
+                        </Card>
+                        <Card className="card-project">
+                            <div class="card-body">
+                                <div class="images">
+                                    <i class="far fa-folder-open fa-7x"></i>
+                                    <a href="https://github.com/mattmkim/penn-safety" target="_blank"> 
+                                        <i class="fab fa-github fa-5x"></i>
+                                    </a>
+                                </div>
+                                <div class="card-info">
+                                    <h4 class="card-title">Penn Safety</h4>
+                                    <p>Final project for CIS 350: Software Engineering. Platform to centralize student submitted tips and alerts.</p>
+                                    <p class="card-tech">Tech Used: Javascript, Node, Express, MongoDB, Google Maps API</p>
+                                </div>
+                            </div>
+                        </Card>
+                    </div>
 
-                    <Card className="project-card">
-                        <div class="project-image-wrapper">
-                            <Image src={safety} className="image" />
-                        </div>
-                        <div class="info-wrapper">
-                            <h3><span class="project-name"> Penn Safety </span></h3>
-                            <p class="project-info">Final project for CIS 350: Software Engineering. Platform to centralize student submitted tips and alerts.</p>
-                            <p class="project-info"><span class="tech-used">Tech Used: </span>Javascript, Node, Express, HTML, CSS, MongoDB, Google Maps API</p>
-                            <Button href="https://github.com/mattmkim/penn-safety">View Project <i class="fas fa-external-link-alt"></i></Button>
-                        </div>
-                    </Card>
+                    <div class="project-row">
+                        <Card className="card-project">
+                            <div class="card-body">
+                                <div class="images">
+                                    <i class="far fa-folder-open fa-7x"></i>
+                                    <a href="https://github.com/mattmkim/rcf-ladders-chatbot" target="_blank"> 
+                                        <i class="fab fa-github fa-5x"></i>
+                                    </a>
+                                </div>
+                                <div class="card-info">
+                                    <h4 class="card-title">Facebook Messenger Chatbot</h4>
+                                    <p>Built a chatbot using the Messenger Platform to help facilitate new relationships in my community. The bot polls and pairs users to meet with each other weekly.</p>
+                                    <p class="card-tech">Tech Used: Javascript, Node, Express, EJS, MongoDB</p>
+                                </div>
+                            </div>
+                        </Card>
+                        <Card className="card-project">
+                            <div class="card-body">
+                                <div class="images">
+                                    <i class="far fa-folder-open fa-7x"></i>
+                                    <a href="https://github.com/mattmkim/ppi-filtering" target="_blank"> 
+                                        <i class="fab fa-github fa-5x"></i>
+                                    </a>
+                                </div>
+                                <div class="card-info">
+                                    <h4 class="card-title">Protein-Protein Interface Filtering</h4>
+                                    <p>Developed naive and machine learning approach to filtering out false protein-protein interface predictions. Developed clustering algorithm to consider both spatial and chemical properties of atoms.</p>
+                                    <p class="card-tech">Tech Used: Python, Pandas, Scipy, Pytorch, VMD</p>
+                                </div>
+                            </div>
+                        </Card>
+                    </div>
 
-                    <Card className="project-card">
-                        <div class="project-image-wrapper">
-                            <Image src={chatbot} className="image" />
-                        </div>
-                        <div class="info-wrapper">
-                            <h3><span class="project-name"> Facebook Messenger Chatbot </span></h3>
-                            <p class="project-info">Built a chatbot to help facilitate new relationships in my community; algorithm pairs users to meet with each other weekly.</p>
-                            <p class="project-info"><span class="tech-used">Tech Used: </span>Javascript, Node, Express, HTML, CSS, EJS, MongoDB</p>
-                            <Button href="https://github.com/mattmkim/rcf-ladders-chatbot">View Project <i class="fas fa-external-link-alt"></i></Button>
-                        </div>
-                    </Card>
+                    <div class="project-row">
+                        <Card className="card-project">
+                            <div class="card-body">
+                                <div class="images">
+                                    <i class="far fa-folder-open fa-7x"></i>
+                                    <a href="https://github.com/mattmkim/facebook-clone" target="_blank"> 
+                                        <i class="fab fa-github fa-5x"></i>
+                                    </a>
+                                </div>
+                                <div class="card-info">
+                                    <h4 class="card-title">Facebook Clone</h4>
+                                    <p>Final project for NETS 212: Scalable and Cloud Computing. Built a Facebook replica with two other classmates. </p>
+                                    <p class="card-tech">Tech Used: Javascript, React, Node, Express, AWS, EC2, MapReduce</p>
+                                </div>
+                            </div>
+                        </Card>
+                        <Card className="card-project">
+                            <div class="card-body">
+                                <div class="images">
+                                    <i class="far fa-folder-open fa-7x"></i>
+                                    <a href="https://github.com/mattmkim/allnba-prediction" target="_blank"> 
+                                        <i class="fab fa-github fa-5x"></i>
+                                    </a>
+                                </div>
+                                <div class="card-info">
+                                    <h4 class="card-title">All-NBA Team Prediction</h4>
+                                    <p>Developed a machine learning model using SVMs to predict the All-NBA teams.</p>
+                                    <p class="card-tech">Tech Used: Python, Pandas, Scikit-Learn, Matplotlib, Seaborn</p>
+                                </div>
+                            </div>
+                        </Card>
+                    </div>
 
-                    <Card className="project-card">
-                        <div class="project-image-wrapper">
-                            <Image src={facebook} className="image" />
-                        </div>
-                        <div class="info-wrapper">
-                            <h3><span class="project-name"> Facebook Clone </span></h3>
-                            <p class="project-info">Final project for NETS 212: Scalable and Cloud Computing.</p>
-                            <p class="project-info"><span class="tech-used">Tech Used: </span>React, Node, Express, Javascript, HTML, CSS, Hadoop MapReduce, DynamoDB</p>
-                            <Button href="https://github.com/mattmkim/facebook-clone">View Project <i class="fas fa-external-link-alt"></i></Button>
-                        </div>
-                    </Card>
-
-                    <Card className="project-card">
-                        <div class="project-image-wrapper">
-                            <Image src={ppi} className="image" />
-                        </div>
-                        <div class="info-wrapper">
-                            <h3><span class="project-name"> Protein-Protein Interface Filtering </span></h3>
-                            <p class="project-info">Developed naive and machine learning approach to filtering out false protein-protein interface predictions. Developed clustering algorithm to consider both spatial and chemical properties of atoms.</p>
-                            <p class="project-info"><span class="tech-used">Tech Used: </span>Python, Pandas, Scipy, Pytorch, VMD</p>
-                            <Button href="https://github.com/mattmkim/minesweeper">View Project <i class="fas fa-external-link-alt"></i></Button>
-                        </div>
-                    </Card>
-
-                    <Card className="project-card">
-                        <div class="project-image-wrapper">
-                            <Image src={bball} className="image" />
-                        </div>
-                        <div class="info-wrapper">
-                            <h3><span class="project-name"> All-NBA Team Prediction </span></h3>
-                            <p class="project-info">Developed a machine learning model using SVMs to predict the All-NBA teams.</p>
-                            <p class="project-info"><span class="tech-used">Tech Used: </span>Python, Pandas, Scikit-Learn, Matplotlib, Seaborn</p>
-                            <Button href="https://github.com/mattmkim/allnba-prediction">View Project <i class="fas fa-external-link-alt"></i></Button>
-                        </div>
-                    </Card>
-
-
-                    <Card className="project-card">
-                        <div class="project-image-wrapper">
-                            <Image src={minesweeper} className="image" />
-                        </div>
-                        <div class="info-wrapper">
-                            <h3><span class="project-name"> Minesweeper </span></h3>
-                            <p class="project-info">Implementation of classic minesweeper game.</p>
-                            <p class="project-info"><span class="tech-used">Tech Used: </span>Java, Java Swing</p>
-                            <Button href="https://github.com/mattmkim/minesweeper">View Project <i class="fas fa-external-link-alt"></i></Button>
-                        </div>
-                    </Card>
-
+                    <div class="project-row">
+                        <Card className="card-project">
+                            <div class="card-body">
+                                <div class="images">
+                                    <i class="far fa-folder-open fa-7x"></i>
+                                    <a href="https://github.com/mattmkim/minesweeper" target="_blank"> 
+                                        <i class="fab fa-github fa-5x"></i>
+                                    </a>
+                                </div>
+                                <div class="card-info">
+                                    <h4 class="card-title">Minesweeper</h4>
+                                    <p>Implementation of the classic minesweeper game.</p>
+                                    <p class="card-tech">Tech Used: Java, Java Swing</p>
+                                </div>
+                            </div>
+                        </Card>
+                    </div>
                 </Container>
             </div>
         );
